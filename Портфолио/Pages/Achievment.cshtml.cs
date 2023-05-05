d3d308ad8e8fd646e38143180c094668c8e5c856
Игорь Гончар <gonchar.goga@bk.ru>
2023-05-05 19:13:38 +0300

    Отправить

---
+++ Портфолио/Pages/Achievment.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Портфолио.Pages
+{
+    public class AchievmentModel : PageModel
+    {
+        private readonly ILogger<AchievmentModel> _logger;
+
+        public AchievmentModel(ILogger<AchievmentModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
